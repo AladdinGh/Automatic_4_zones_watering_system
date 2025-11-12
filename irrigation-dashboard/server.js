8b4b0ec1dc551bb433f745d41ac9f2fbb6b602cb
AladdinGh <gh.aladdin@gmail.com>
2025-11-12 16:32:19 +0100

    esp published, web inf displays

---
+++ irrigation-dashboard/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mqtt = require('mqtt');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const mqttBroker = 'mqtt://192.168.178.45';
+const topicMoisture = 'irrigation/moisture';
+
+const client = mqtt.connect(mqttBroker);
+
+// Cache the last received moisture message
+let lastMoisture = null;
+
+// Connect to MQTT broker
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  client.subscribe(topicMoisture, () => console.log('Subscribed to moisture topic'));
+});
+
+// Receive MQTT messages and broadcast via Socket.IO
+client.on('message', (topic, message) => {
+  lastMoisture = message.toString();
+  io.emit('update', lastMoisture);
+});
+
+// Serve HTML from "public" folder
+app.use(express.static('public'));
+
+// Handle Socket.IO connections
+io.on('connection', socket => {
+  console.log('Client connected');
+  
+  // Send last cached moisture values immediately
+  if (lastMoisture) {
+    socket.emit('update', lastMoisture);
+  }
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
